199e7b109068e698a96276042244533bdc5cf30c
Vin Gabriel Fillon <53989590+Shinixagami@users.noreply.github.com>
2022-06-27 05:54:38 +0000

    +FIRST save

--- main.cpp
+++ main.cpp
@@ -7,13 +7,6 @@ int main() {
    
    getline(cin, strInput);  //this doesnt seem right
 
-// its what the book wrote
-
-  //its tankig in enter as well, you need a terminate -there's better way I think. 
-// OHHHHHHHHH IT WORKS NOWW I GOT IT YES// I WILL GO TO NEXT PROBLEM THANK YOU THANK YOU
-
-  //your cin input is wronng  - you need to fix it. 
-
    //use == NOT =
   //also MAKE ELSE IF statements like this
    if (strInput.at(strInput.size() - 1) =='?') {   
